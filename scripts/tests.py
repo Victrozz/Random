5f5c830cf3364f6036b3a685bf42d198d2266d0c
Victrozz <victorjuval@gmail.com>
2021-12-29 20:54:41 +0100

    added another print statement to tests.py to figure all this out

--- scripts/tests.py
+++ scripts/tests.py
@@ -1,1 +1,2 @@
-print("i have no idea how all this github stuff works...")
+print("i have no idea how all this github stuff works...") # this was written from the web
+print("i am starting to get the hang of it, still not working on my home pc though...") # this was written from miguels old laptop
