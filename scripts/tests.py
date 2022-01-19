385d0cc083b2fbd50bfdfdb41c3933cb2173fa7e
Victrozz <80338823+Victrozz@users.noreply.github.com>
2022-01-19 21:30:28 +0100

    updated tests.py

--- scripts/tests.py
+++ scripts/tests.py
@@ -1,2 +1,3 @@
 print("i have no idea how all this github stuff works...") # this was written from the web
 print("i am starting to get the hang of it, still not working on my home pc though...") # this was written from miguels old laptop
+print("got it working on my home pc a while ago, just forgot to update this :P") # this was written from my home pc
